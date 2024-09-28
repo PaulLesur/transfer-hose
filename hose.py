a2a09a99d0ad90d772a1c8ee8b52b574ca80c865
Paul LESUR <paul.lesur@free.fr>
2024-09-28 11:18:15 +0200

    Send data

--- hose.py
+++ hose.py
@@ -1,3 +1,4 @@
+import sys
 import logging
 import requests
 import yaml
@@ -15,7 +16,6 @@ def read_yaml(file_yaml):
             print(f"Error when reading yaml file: {e}")
             return None
 
-
 def get_metric_value(config, metric_name: str) -> str:
     """
     Returns the value of a metric, fetched in a Prometheus server.
@@ -23,24 +23,28 @@ def get_metric_value(config, metric_name: str) -> str:
     """
     logger.info("Getting metric value for %s", metric_name)
     logger.info( f"GET {config['ispindel']['prometheus']['host']}:{config['ispindel']['prometheus']['port']}/api/v1/query?query={metric_name}")
-    fetched_data = requests.get(
-        f"{config['ispindel']['prometheus']['host']}:{config['ispindel']['prometheus']['port']}/api/v1/query?query={metric_name}",
-        timeout=10
-        )
-    #TODO find a better way to retrieve this data
-    logger.info(fetched_data.json()['data']['result'][0]['value'][1])
+    try:
+        fetched_data = requests.get(
+            f"{config['ispindel']['prometheus']['host']}:{config['ispindel']['prometheus']['port']}/api/v1/query?query={metric_name}",
+            timeout=10
+            )
+        #TODO find a better way to retrieve this data
+        logger.info(fetched_data.json()['data']['result'][0]['value'][1])
+    except KeyError:
+        logger.error("Unable to fetch data, exiting...")
+        sys.exit(1)
     return fetched_data.json()['data']['result'][0]['value'][1]
 
-def format_payload_for_littlebock(temperature: str, battery: str, gravity: str) -> str:
-    """
-    Takes informations to send and format them into an objet to send to Littlebock
-    """
-    # Peut etre inutile si je fais ça proprement avec une structure de donnée appropriée
-    return ""
-
-def send_data_to_littlebock(temperature: str, battery: str, gravity: str):
+def send_data_to_littlebock(config, data: object):
     """
     Takes informations and send them to Littlebock
     """
-    logger.info("Sending infos to Littlebock : temperature=%s , battery=%s , gravity=%s", temperature, battery, gravity)
-    return
+    url = f"{config['ispindel']['littlebock']['serverAdress']}{config['ispindel']['littlebock']['serverUrl']}"
+    logger.info("Sending data to Littlebock : %s , %s", url, data)
+    response = requests.post(url, data, timeout=10)
+
+    if "not attached" in response.json()['message']:
+        logger.error("This device is not attached to a brew session. Exiting...")
+        sys.exit(2)
+    else:
+        logger.info("data sent to littlebock")
--- main.py
+++ main.py
@@ -8,14 +8,17 @@ def main():
     """Retrieve data from Prometheus server and send it ot Littlebock"""
     logger.info('Transfer-Hose started !')    
 
-    # Exemple d'utilisation
     file = 'configuration/config.yaml'
     config = hose.read_yaml(file)
     if config:
         logger.info("Loaded configuration : %s", config)
         # TODO stuff
-
-        hose.get_metric_value(config=config, metric_name="temperature")
+        data_to_send = {
+            "temperature" : hose.get_metric_value(config=config, metric_name="temperature"),
+            "gravity" : hose.get_metric_value(config=config, metric_name="gravity"),
+            "battery" : hose.get_metric_value(config=config, metric_name="battery")
+        }
+        hose.send_data_to_littlebock(config=config, data=data_to_send)
 
     logger.info('Transfer-Hose exited !')
 
